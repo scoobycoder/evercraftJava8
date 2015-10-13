c62b120220bae7f46a448da212d5653a993bc5e9
Nate <nathan_halterman@gap.com>
2015-10-13 10:37:48 -0400

    basic horrible pojo with getter setter pattern for abilities, this is so that I can see if I can move this into spring and clean this up

---
+++ src/main/java/evercraftJava8/Abilities.java
@@ -0,0 +1,49 @@
+package evercraftJava8;
+
+public class Abilities {
+
+	private int strength = 10;
+	private int charisma = 0;
+	private int dexterity = 5;
+	private int constitution = 7;
+	private int wisdom = 15;
+	private int intelligence = 20;
+
+	public int getStrength() {
+		return strength;
+	}
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+	public int getDexterity() {
+		return dexterity;
+	}
+	public void setDexterity(int dexterity) {
+		this.dexterity = dexterity;
+	}
+	public int getConstitution() {
+		return constitution;
+	}
+	public void setConstitution(int constitution) {
+		this.constitution = constitution;
+	}
+	public int getWisdom() {
+		return wisdom;
+	}
+	public void setWisdom(int wisdom) {
+		this.wisdom = wisdom;
+	}
+	public int getIntelligence() {
+		return intelligence;
+	}
+	public void setIntelligence(int intelligence) {
+		this.intelligence = intelligence;
+	}
+	public int getCharisma() {
+		return charisma;
+	}
+	public void setCharisma(int charisma) {
+		this.charisma = charisma;
+	}
+
+}
---
+++ src/test/java/evercraftJava8/AbilitiesTest.java
@@ -0,0 +1,34 @@
+package evercraftJava8;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class AbilitiesTest {
+
+	private ClassPathXmlApplicationContext applicationContext;
+	
+	@InjectMocks
+	private Abilities underTest;
+	
+	@Before
+	public void setup() {
+		applicationContext = new ClassPathXmlApplicationContext("application-context.xml");
+		underTest = (Abilities) applicationContext.getBean("abilities");
+	}
+	
+	@Test
+	public void abilitiesShouldIncludeAllAbilities() {
+		assertThat(underTest.getStrength(), is(10));
+		assertThat(underTest.getDexterity(), is(5));
+		assertThat(underTest.getConstitution(), is(7));
+		assertThat(underTest.getWisdom(), is(15));
+		assertThat(underTest.getIntelligence(), is(20));
+		assertThat(underTest.getCharisma(), is(0));
+	}
+	
+}
